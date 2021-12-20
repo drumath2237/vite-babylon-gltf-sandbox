6579f1569df88d3bd684479dfb0bcc6e47fffa9d
drumath <2237k.drumath@gmail.com>
2021-12-20 20:50:59 +0900

    :sparkles: scssも整えてシーンも整えてセットアップ完了

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import * as BABYLON from '@babylonjs/core';
 
-import './style.css';
+import './style.scss';
 
 const renderCanvas = <HTMLCanvasElement>document.getElementById('renderCanvas');
 
@@ -8,9 +8,16 @@ const main = (canvas: HTMLCanvasElement) => {
   const engine = new BABYLON.Engine(canvas, true);
   const scene = new BABYLON.Scene(engine);
 
-  scene.createDefaultCameraOrLight(true, true, true);
+  const camera = new BABYLON.UniversalCamera('camera', new BABYLON.Vector3(2, 2, 2), scene);
+  camera.target = new BABYLON.Vector3(0, 0.5, 0);
+  camera.attachControl(true);
+
+  const light = new BABYLON.DirectionalLight('light', new BABYLON.Vector3(-1, -3, -1.5), scene);
+  light.intensity = 1.0;
+
   scene.createDefaultEnvironment();
-  BABYLON.MeshBuilder.CreateBox('box', {}, scene);
+  const cube = BABYLON.MeshBuilder.CreateBox('box', {}, scene);
+  cube.position = new BABYLON.Vector3(0, 0.5, 0);
 
   engine.runRenderLoop(() => {
     scene.render();
